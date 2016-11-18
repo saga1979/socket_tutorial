d09a2e0a47c0b736e8cb7ee64d670c9a544c346b
ZhangFeng <saga28@yeah.net>
2016-11-18 15:10:11 +0800

    修改了几处编译错误

--- simple_udp_client.c
+++ simple_udp_client.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define BUFLEN 512
 #define NPACK 10
@@ -17,20 +18,20 @@ void on_err(char *s)
 	exit(1);
 }
 
-int main(void)
-{
-	
+int main(int argc, char** argv)
+{	
 	int s = -1;
+	struct sockaddr_in si_other;
 	int  slen = sizeof(si_other);
 	char buf[BUFLEN];
 
 	if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-		on_err(strerror(errno);
-	struct sockaddr_in si_other;
+		on_err(strerror(errno));
+	
 	memset((char *)&si_other, 0, sizeof(si_other));
 	si_other.sin_family = AF_INET;
-	si_other.sin_port = htons(PORT);
-	if (inet_aton(SRV_IP, &si_other.sin_addr) == 0) {
+	si_other.sin_port = htons(atoi(argv[2]));
+	if (inet_aton(argv[1], &si_other.sin_addr) == 0) {
 		fprintf(stderr, "inet_aton() failed\n");
 		exit(1);
 	}
@@ -39,8 +40,8 @@ int main(void)
 	{
 		printf("Sending packet %d\n", i);
 		sprintf(buf, "This is packet %d\n", i);
-		if (sendto(s, buf, BUFLEN, 0, &si_other, slen) == -1)
-			on_err(strerror(errno);
+		if (sendto(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, slen) == -1)
+			on_err(strerror(errno));
 	}
 
 	close(s);
--- simple_udp_server.c
+++ simple_udp_server.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define BUFLEN 512
 #define NPACK 10
@@ -20,23 +21,23 @@ void on_err(char *s)
 int main(void)
 {	
 	int s = -1;
+	struct sockaddr_in si_me, si_other;
 	int slen = sizeof(si_other);
 	char buf[BUFLEN];
 
 	if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
 		on_err(strerror(errno));
-
-	struct sockaddr_in si_me, si_other;
+	
 	memset((char *)&si_me, 0, sizeof(si_me));
 	si_me.sin_family = AF_INET;
 	si_me.sin_port = htons(PORT);
 	si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-	if (bind(s, &si_me, sizeof(si_me)) == -1)
+	if (bind(s, (struct sockaddr*)&si_me, sizeof(si_me)) == -1)
 		on_err(strerror(errno));
 
 	for (int i = 0; i < NPACK; i++)
 	{
-		if (recvfrom(s, buf, BUFLEN, 0, &si_other, &slen) == -1)
+		if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr * )&si_other, &slen) == -1)
 			on_err(strerror(errno));
 		printf("Received packet from %s:%d\nData: %s\n\n",
 			inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port), buf);
